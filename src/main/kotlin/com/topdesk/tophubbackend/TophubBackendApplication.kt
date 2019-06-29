3653482fbe7ccc58e9115e627b97ca6e5043eb27
Peter Aichinger <p.aichinger@googlemail.com>
2019-06-29 16:16:57 +0200

    First commit

---
+++ src/main/kotlin/com/topdesk/tophubbackend/TophubBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.topdesk.tophubbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TophubBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<TophubBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/topdesk/tophubbackend/TophubBackendApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.topdesk.tophubbackend
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TophubBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
