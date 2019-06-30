4d83bd579b9290ba9134a45e01afc220bcc254e7
Peter Aichinger <p.aichinger@googlemail.com>
2019-06-30 12:17:40 +0200

    Basic graphql-setup

---
+++ src/main/kotlin/com/topdesk/tophubbackend/graphql/resolvers/CategoryQueryResolver.kt
@@ -0,0 +1,13 @@
+package com.topdesk.tophubbackend.graphql.resolvers
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import com.topdesk.tophubbackend.data.entities.CategoryRepository
+import org.springframework.stereotype.Component
+
+
+@Component
+class CategoryQueryResolver(
+        private val categoryRepository: CategoryRepository
+) : GraphQLQueryResolver {
+    fun getCategoryById(id: Int) = categoryRepository.findById(id)
+}
