72ef163e749b6db30d6de7484fbda06cd843b571
Peter Aichinger <p.aichinger@googlemail.com>
2019-07-22 08:06:22 +0200

    Add Entry entity to search index

--- src/main/kotlin/com/topdesk/tophubbackend/data/entities/Entry.kt
+++ src/main/kotlin/com/topdesk/tophubbackend/data/entities/Entry.kt
@@ -1,17 +1,22 @@
 package com.topdesk.tophubbackend.data.entities
 
+import org.hibernate.search.annotations.Field
+import org.hibernate.search.annotations.Indexed
 import javax.persistence.*
 
 @Entity
+@Indexed
 data class Entry (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int,
 
     @Column(nullable = false)
+    @Field
     val title: String,
 
     @Column(nullable = false)
+    @Field
     val target: String,
 
     @ManyToMany(mappedBy = "entries")
