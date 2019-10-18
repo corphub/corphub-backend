f7d4fc756b5dbb3e4cc42e5d0d9c895c3d85dc96
Peter Aichinger <p.aichinger@googlemail.com>
2019-10-19 00:08:22 +0200

    feat: introduce snapshot build

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
-latesttag=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo checking out ${latesttag}
-git checkout ${latesttag}
-patch_version=` mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
-echo found patch version: ${patch_version}
-IFS='.' read -r -a versions <<< "$patch_version"
-#IFS='.' read -r -a version <<< ${patch_version}
+BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+echo branch: ${BRANCH}
+if [[ "$BRANCH" == "master" ]]; then
+    latesttag=$(git describe --tags `git rev-list --tags --max-count=1`)
+    echo checking out ${latesttag}
+    git checkout ${latesttag}
+fi
+full_version=` mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
+echo found version: ${full_version}
+IFS='.' read -r -a versions <<< "full_version"
 feature_version=${versions[0]}.${versions[1]}
 major_version=${versions[0]}
 echo found feature version: ${feature_version}
 echo found major version: ${major_version}
 
-docker build --cache-from corphub/corphub-backend --build-arg POM_VERSION=${patch_version} -t corphub/corphub-backend:${patch_version} -t corphub/corphub-backend:${feature_version} -t corphub/corphub-backend:${major_version} -t corphub/corphub-backend:latest .
-docker push corphub/corphub-backend
+if [[ "$BRANCH" == "master" ]]; then
+    echo building release version
+    docker build --cache-from corphub/corphub-backend --build-arg POM_VERSION=${full_version} -t corphub/corphub-backend:${full_version} -t corphub/corphub-backend:${feature_version} -t corphub/corphub-backend:${major_version} -t corphub/corphub-backend:latest .
+    docker push corphub/corphub-backend
+fi
+if [[ "$BRANCH" == "dev" ]]; then
+    echo building snapshot version
+    docker build --cache-from corphub/corphub-backend --build-arg POM_VERSION=${full_version} -t corphub/corphub-backend:snapshot .
+    docker push corphub/corphub-backend
+fi
+
