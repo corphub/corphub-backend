235eb0e045f72ba33f170844c5f8ec93a34611e6
Peter Aichinger <p.aichinger@googlemail.com>
2019-10-18 23:19:27 +0200

    feat: overhaul deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,16 @@
-#!/usr/bin/env bash
-docker-ci-deploy --tag latest --tag $(git describe --tags $(git rev-list --tags --max-count=1)) corphub/corphub-backend:latest
+#!/bin/sh
+latesttag=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo checking out ${latesttag}
+git checkout ${latesttag}
+
+patch_version=` mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
+echo found patch version: ${patch_version}
+IFS='.' read -r -a versions <<< "$patch_version"
+#IFS='.' read -r -a version <<< ${patch_version}
+feature_version=${versions[0]}.${versions[1]}
+major_version=${versions[0]}
+echo found feature version: ${feature_version}
+echo found major version: ${major_version}
+
+docker build --cache-from corphub/corphub-backend --build-arg POM_VERSION=${patch_version} -t corphub/corphub-backend:${patch_version} -t corphub/corphub-backend:${feature_version} -t corphub/corphub-backend:${major_version} -t corphub/corphub-backend:latest .
+docker push corphub/corphub-backend
--- mvnbuild.sh
+++
@@ -1,2 +0,0 @@
-mvn install -DskipTests
-cp target/tophub-backend-*.jar tophub-backend.jar
