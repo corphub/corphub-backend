62824dc06161c9681c33aeb0bd620e6b836765d8
Peter Aichinger <p.aichinger@googlemail.com>
2020-01-30 13:29:18 +0100

    remove existing graphql setup

--- src/main/kotlin/com/topdesk/tophubbackend/graphql/resolvers/VersionQueryResolver.kt
+++
@@ -1,9 +0,0 @@
-package com.topdesk.tophubbackend.graphql.resolvers
-
-import com.coxautodev.graphql.tools.GraphQLQueryResolver
-import org.springframework.stereotype.Component
-
-@Component
-class VersionQueryResolver : GraphQLQueryResolver {
-    fun version() = "1.0.0"
-}
